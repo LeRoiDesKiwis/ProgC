e4cea522b4a44923d2f1eb3b1d1aee786bdbd438
LeRoiDesKiwis <ferryantony1@gmail.com>
2023-03-09 10:54:12 +0100

    add étape 2

---
+++ Etape2Tri/ListeDouble/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "tri.h"
+
+int main()
+{
+    T_liste l;
+    printf("Zone de test de vos fonctions!\n");
+    printf("1 fonction = 1 test immediat!\n");
+
+    return 0;
+}
---
+++ Etape2Tri/ListeDouble/tri.h
@@ -0,0 +1,9 @@
+#ifndef LISTEDOUBLE_H_INCLUDED
+#define LISTEDOUBLE_H_INCLUDED
+
+#include <stdbool.h>
+
+
+
+#endif // LISTEDOUBLE_H_INCLUDED
+
