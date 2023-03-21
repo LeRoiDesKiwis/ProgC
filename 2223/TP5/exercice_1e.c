ecb30dd9c3a9a005dc60814b298ddb8294307185
LeRoiDesKiwis <ferryantony1@gmail.com>
2023-03-21 16:38:56 +0100

    exercice 1e

--- 2223/TP5/exercice_1e.c
+++ 2223/TP5/exercice_1e.c
@@ -11,7 +11,7 @@
 
 // variable globale pour alléger le code
 // TODO déclarer les sémaphores/mutex ici
-
+Semaphore sem;
 
 /*
  * même code pour tous les fils
@@ -23,11 +23,13 @@ void fils(int numero)
     sleep(1 + random() % 3);
 
     // Début de la section critique
+    sema_prendre(sem);
     printf("fils %d : est entré en section critique\n", numero);
     sleep(3);
     printf("fils %d : va sortir de la section critique\n", numero);
     // fin de la section critique
- 
+    sema_vendre(sem);
+
     // pour ne pas revenir dans le main
     exit(EXIT_SUCCESS);
 }
@@ -43,13 +45,14 @@ int main()
     // à surtout créer avant le fork,
     // pas de mutex car la valeur peut être supérieure à 1
     // TODO création sémaphore
+    sem = sema_creer(K);
 
     for (int i = 0; i < N; i++)
     {
         if (fork() == 0)
             fils(i+1);        // rappel on ne sort pas de la fonction
     }
-    
+
     // ne pas oublier (rappel uniquement le père exécute ce code)
     for (int i = 0; i < N; i++)
         wait(NULL);
@@ -58,6 +61,7 @@ int main()
     // à faire absolument après le wait, sinon on risque de le
     // détruire alors que le fils s'en sert encore
     // TODO destruction mutex/semaphore
- 
+    sema_detruire(&sem);
+
     return EXIT_SUCCESS;
 }
